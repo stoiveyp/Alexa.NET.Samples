08d7514954d37fe79d8eaa2ad0ef24c9616d1d23
Steven Pears <steven.pears@live.co.uk>
2018-05-05 21:10:54 +0100

    InitialQuizOutlay

---
+++ SimpleGadgetQuiz/Function.cs
@@ -0,0 +1,78 @@
+using System.Threading.Tasks;
+using Alexa.NET;
+using Alexa.NET.Gadgets.GameEngine;
+using Alexa.NET.Gadgets.GameEngine.Requests;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+using Alexa.NET.Response.Ssml;
+using Alexa.NET.StateManagement;
+using Alexa.NET.Response.Ssml.SoundLibrary;
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace SimpleGadgetQuiz
+{
+    public class Function
+    {
+        public Function()
+        {
+            new GadgetRequestHandler().AddToRequestConverter();
+        }
+
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task<SkillResponse> FunctionHandler(SkillRequest input, ILambdaContext context)
+        {
+            var state = new SkillState(input);
+
+            switch (input.Request)
+            {
+                case LaunchRequest _:
+                    var rollCallResponse = ResponseBuilder.Ask("Please press the buttons you want to use for this quiz", null);
+                    rollCallResponse.AddRollCall(20000, "first", "second");
+
+                    return rollCallResponse;
+                case InputHandlerEventRequest inputHandler:
+                    if (inputHandler.TryRollCallResult(out var mapping, "first", "second"))
+                    {
+                        state.SetSession("player1", mapping["first"]);
+                        state.SetSession("player2", mapping["second"]);
+                        var question = "Question One. Which is the bigger number, 3 or 5?";
+
+                        var response = ResponseBuilder.Ask($"Okay then, let's begin! {question}", null);
+                        response.WhenFirstButtonDown(mapping, "buzzedIn", 10000);
+                        return response;
+                    }
+
+                    if (inputHandler.TryMapEventGadget("buzzedIn", out var player))
+                    {
+                        var buzzedInPlayer = player == state.GetSession<string>("player1") ? "player one" : "player two";
+                        state.SetSession("currentAnswer", "five");
+                        return ResponseBuilder.Ask($"Okay then {buzzedInPlayer}, what's your answer?", null);
+                    }
+
+                    break;
+                case IntentRequest intent:
+                    if (intent.Intent.Name.ToLower() == "answer")
+                    {
+                        Speech responseSpeech = intent.Intent.Slots["answer"].Value == await state.Get<string>("answer") ? 
+                            new Speech(Human.CrowdCheerMedium,new PlainText("Well done, that's correct!")) :
+                            new Speech(Human.CrowdBoo01, new PlainText("Well done, that's correct!"));
+
+                        return ResponseBuilder.Tell(responseSpeech);
+                    }
+
+                    break;
+            }
+
+            return ResponseBuilder.Tell("Sorry - I'm not sure how to handle that");
+        }
+    }
+}
