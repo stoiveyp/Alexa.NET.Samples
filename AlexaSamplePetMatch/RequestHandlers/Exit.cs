a5585397da5b6999e3060e7ddaddb407a41a848d
Steven Pears <steven.pears@live.co.uk>
2018-06-21 00:15:48 +0100

    Add extra samples (#7)
    * Launch and mythical
    
    * fix skill build
    
    * completed handler
    
    * fix dialog state
    
    * fix query parameter in api call
    
    * Help and exit handlers

---
+++ AlexaSamplePetMatch/RequestHandlers/Exit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Alexa.NET.Request.Type;
+using Alexa.NET.RequestHandlers;
+using Alexa.NET.RequestHandlers.Handlers;
+using Alexa.NET.Response;
+
+namespace AlexaSamplePetMatch.RequestHandlers
+{
+    public class Exit:SynchronousRequestHandler
+    {
+        public override bool CanHandle(RequestInformation information)
+        {
+            if (information.SkillRequest.Request is IntentRequest intent)
+            {
+                return intent.Intent.Name == BuiltInIntent.Stop || intent.Intent.Name == BuiltInIntent.Cancel;
+            }
+
+            return false;
+        }
+
+        public override SkillResponse HandleSyncRequest(RequestInformation information)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
